97b05378b4bb87e01ccc4e3fb71ce1bddf87adc5
Karen <karen.nikoghosyan.her@gmail.com>
2024-07-16 11:11:31 +0400

    Frontend is ready

--- src/App.js
+++ src/App.js
@@ -14,13 +14,85 @@ import TokenMaster from './abis/TokenMaster.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [tokenMaster, setTokenMaster] = useState(null)
+  const [occasions, setOccasions] = useState([])
+
+  const [occasion, setOccasion] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const loadBlockchainData = async () => {
+    // Creating smart contract instance in js
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider )
+
+    const network = await provider.getNetwork()    
+    const contractAddress = config[network.chainId].TokenMaster.address
+    const tokenMaster = new ethers.Contract(contractAddress, TokenMaster, provider)
+    setTokenMaster(tokenMaster)
+    // console.log(network)
+
+    // Including occasions from Smart contract
+
+    const totalOccasions = await tokenMaster.totalOccasions()
+    const occasions = []
+    for(let i = 1; i <= totalOccasions; i++) {
+      const occasion = await tokenMaster.getOccasion(i)
+      occasions.push(occasion)
+    }
+    setOccasions(occasions)
+    // console.log(occasions)
+
+    // Refresh Account
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'})
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)      
+    })
+
+
+  }
+
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
       <header>
-
-        <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
+        <Navigation account={account} setAccount={setAccount} />
+        <h2 className='header__title'><strong>Event</strong> Tickets</h2>
       </header>
+
+      <Sort />
+
+      <div className='cards'>
+        {occasions.map((occasion, index) => (
+          <Card
+            occasion={occasion}
+            id={index + 1}
+            tokenMaster={tokenMaster} 
+            provider={provider} 
+            account={account} 
+            toggle={toggle} 
+            setToggle={setToggle} 
+            setOccasion={setOccasion}
+            key={index}
+          />
+        ))}        
+      </div>
+
+      {toggle && (
+        <SeatChart
+          occasion={occasion}
+          tokenMaster={tokenMaster}
+          provider={provider}
+          setToggle={setToggle}
+        />
+      )}
+
 
     </div>
   );
